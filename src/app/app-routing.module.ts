a764ebfde8225be163eec55c5edc27abcc8426e1
PedroUribe-12 <orfeouribe@gmail.com>
2022-04-07 02:12:29 -0300

    se agrega el proyecto1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'contactanos',
+    component: ContactanosComponent
+  },
+  {
+    path:'empleados',
+    component: EmpleadosComponent
+  },
+  {
+    path:'productos',
+    component: ProductosComponent
+  },
+  {
+    path:'sobre-nosotros',
+    component: SobreNosotrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegadorComponent,
+    FooterComponent,
+    EmpleadosComponent,
+    ContactanosComponent,
+    SobreNosotrosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
